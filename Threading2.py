7197580b9f83c7104c3e5c8787dd073f73c922ec
muzy06038 <sub06038@naver.com>
2021-07-26 15:44:19 +0900

    thread

--- Threading2.py
+++ Threading2.py
@@ -6,4 +6,22 @@ def get_logger():
     logger.setLevel(logging.DEBUG)
 
     fh = logging.StreamHandler()
-    fmt = ''
+    fmt = '%(asctime)s - %(threadName)s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(fmt)
+    fh.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    return logger
+
+def execute(number, logger):
+
+    logger.debug('execute function executing')
+    result = number * 2
+    logger.debug(f'execute function ended with: {result}')
+
+if __name__ == '__main__':
+    logger = get_logger()
+    for i, name in enumerate(['Kim','Lee','Park','Cho','Hong']):
+        my_thread = threading.Thread(
+            target=execute, name=name, args=(i,logger))
+        my_thread.start()
